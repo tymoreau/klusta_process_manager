476edc78d05d2f085049ddc5b2c8969a2a981eb9
tymoreau <typhaine.moreau@insa-lyon.fr>
2015-07-07 11:15:37 +0200

    separate fileBrowser

---
+++ klusta_process_manager/fileBrowser/folderView.py
@@ -0,0 +1,76 @@
+#QT
+import sip
+sip.setapi('QVariant',2)
+sip.setapi('QString',2)
+from PyQt4 import QtCore,QtGui
+
+class FolderView(QtGui.QWidget):
+
+	def __init__(self,model,parent=None):
+		super(FolderView,self).__init__(parent)
+		
+		#Table (list of experiment)
+		self.table=QtGui.QTableView(self)
+		self.table.horizontalHeader().setVisible(False)
+		self.table.verticalHeader().setVisible(False)
+		self.table.horizontalHeader().setResizeMode(QtGui.QHeaderView.ResizeToContents)
+		self.table.setShowGrid(False)
+		vbar=self.table.verticalScrollBar()
+		self.table.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+		self.table.setModel(model)
+		self.table.selectionModel().selectionChanged.connect(self.on_selection_changed)
+
+		#ListFile (contents of one experiment folder)
+		self.listFile=QtGui.QTreeView(self)
+		self.listFile.header().setResizeMode(QtGui.QHeaderView.ResizeToContents)
+		self.listFile.header().setStretchLastSection(True)
+	
+		#FileSytemModel linked to listFile
+		self.folderModel=QtGui.QFileSystemModel(self)
+		
+		#Layout
+		self.space=QtGui.QWidget()
+		hbox=QtGui.QHBoxLayout()
+		hbox.addWidget(vbar)
+		hbox.addWidget(self.table)
+		hbox.addWidget(self.space)
+		hbox.addWidget(self.listFile)
+		self.setLayout(hbox)
+		self.listFile.hide()
+		self.space.show()
+		
+	def refresh(self):
+		self.listFile.update()
+		
+	#User clicked on one folder
+	def on_selection_changed(self,selected,deselected):
+		if len(selected.indexes())==0:
+			if len(self.table.selectedIndexes())==0:
+				self.listFile.hide()
+				self.space.show()
+				return
+			else:
+				lastIndex=self.table.selectedIndexes()[-1]
+		else:
+			lastIndex=selected.indexes()[-1]
+		self.listFile.show()
+		self.space.hide()
+		#Set ListFile to display folder's content
+		path=lastIndex.model().pathLocal_from_index(lastIndex)
+		self.folderModel.setRootPath(path)
+		self.listFile.setModel(self.folderModel)
+		self.listFile.setRootIndex(self.folderModel.index(path))
+		self.listFile.clearSelection()
+		
+	#user changed animal
+	def reset_view(self):
+		self.table.reset()
+		self.table.clearSelection()
+		self.folderModel.reset()
+		self.listFile.hide()
+		self.listFile.clearSelection()
+		self.space.show()
+		self.table.resizeColumnsToContents()
+		he=self.table.horizontalHeader()
+		length=he.sectionSize(0)+he.sectionSize(1)+he.sectionSize(2)+he.sectionSize(3)
+		self.table.setMaximumWidth(length+10)
---
+++ klusta_process_manager/fileBrowser/tableDelegate.py
@@ -0,0 +1,92 @@
+#QT
+import sip
+sip.setapi('QVariant',2)
+sip.setapi('QString',2)
+from PyQt4 import QtCore,QtGui
+
+class TableDelegate(QtGui.QStyledItemDelegate):
+	def __init__(self,parent=None):
+		super(TableDelegate,self).__init__(parent)
+		self.weekLines=[]
+		self.dayLines=[]
+		self.middleWeek=[]
+		self.middleWeekOdd=[]
+		self.middleDay=[]
+		self.middleDayOdd=[]
+		
+	def reset_horizontal_lines(self,listDate):
+		previousMonth=listDate[0].date().month()
+		previousWeek=listDate[0].date().weekNumber()[0]
+		previousDay=listDate[0].date().day()
+		weekLines=[]
+		dayLines=[]
+		
+		for row,date in enumerate(listDate):
+			month=date.date().month()
+			week=date.date().weekNumber()[0]
+			day=date.date().day()
+			if month==previousMonth and week==previousWeek:     #same week
+				if day!=previousDay:
+					dayLines.append(row-1)
+			else:
+				weekLines.append(row-1)
+			previousMonth=month
+			previousWeek=week
+			previousDay=day
+		weekLines.append(len(listDate)-1)
+		
+		self.weekLines=weekLines
+		self.dayLines=dayLines
+		
+		week2=[-1]+weekLines[:-1]
+		middleWeek=[ (a+b+1) for a,b in zip(weekLines,week2)]
+		self.middleWeek=[ int(summ/2) for summ in middleWeek if summ%2==0]
+		self.middleWeekOdd=[ int(summ/2) for summ in middleWeek if summ%2!=0]
+		dayLines=dayLines+weekLines
+		dayLines.sort()
+		day2=[-1]+dayLines[:-1]
+		middleDay=[(a+b+1) for a,b in zip(dayLines,day2)]
+		self.middleDay=[ int(summ/2) for summ in middleDay if summ%2==0]
+		self.middleDayOdd=[ int(summ/2) for summ in middleDay if summ%2!=0]
+
+	def paint(self,painter,option,index):
+		row=index.row()
+		col=index.column()
+		#Vertical Lines
+		if col==2:
+			p1=option.rect.topRight()
+			p2=option.rect.bottomRight()
+			line=QtCore.QLine(p1,p2)
+			painter.setPen(QtCore.Qt.black)
+			painter.drawLine(line)
+		#Horizontal Lines---------------------------------
+		#Month/Year/Week
+		if row in self.weekLines:
+			p1=option.rect.bottomLeft()
+			p2=option.rect.bottomRight()
+			line=QtCore.QLine(p1,p2)
+			painter.setPen(QtCore.Qt.black)
+			painter.drawLine(line)
+		#Day
+		elif col!=0 and (row in self.dayLines):
+			painter.setPen(QtGui.QPen(QtGui.QBrush(QtCore.Qt.gray),1.5,QtCore.Qt.DotLine))
+			p1=option.rect.bottomLeft()
+			p2=option.rect.bottomRight()
+			line=QtCore.QLine(p1,p2)
+			painter.drawLine(line)
+		#Draw Text
+		painter.setPen(QtCore.Qt.black)
+		if col==3:
+			return super(TableDelegate,self).paint(painter,option,index)
+		elif col==0 and (row in self.middleWeek):
+			painter.drawText(option.rect,QtCore.Qt.AlignVCenter,index.data())
+		elif col==0 and (row in self.middleWeekOdd):
+			rowHeight=self.sizeHint(option,index).height()//2 +5
+			option.rect.translate(0,rowHeight)
+			painter.drawText(option.rect,QtCore.Qt.AlignVCenter,index.data())
+		elif (col==1 or col==2) and (row in self.middleDay):
+			painter.drawText(option.rect,QtCore.Qt.AlignVCenter,index.data())
+		elif (col==1 or col==2) and (row in self.middleDayOdd):
+			rowHeight=self.sizeHint(option,index).height()//2 +7
+			option.rect.translate(0,rowHeight)
+			painter.drawText(option.rect,QtCore.Qt.AlignVCenter,index.data())
