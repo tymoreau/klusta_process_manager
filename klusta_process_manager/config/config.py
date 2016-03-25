33fcca4bf35b8e11efb479ab869c61fd326b678d
tymoreau <typhaine.moreau@insa-lyon.fr>
2016-03-25 17:08:18 +0100

    clean up

--- klusta_process_manager/config/config.py
+++ klusta_process_manager/config/config.py
@@ -2,103 +2,92 @@ from __future__ import print_function
 import os
 import json
 
-# Create a folder in the user's root with a config file, scripts and prm/prb models
 def create_user_config_file(override=False):
-	dirPath=get_user_folder_path()
-	if not os.path.exists(dirPath):
-		print("Creating",dirPath)
-		os.mkdir(dirPath)
+    """Create a folder in the user's root with a config file"""
 
-	scriptFolder=os.path.join(dirPath,"scripts")
-	if not os.path.exists(scriptFolder):
-		os.mkdir(scriptFolder)
+    dirPath = get_user_folder_path()
+    if not os.path.exists(dirPath):
+        print("Creating", dirPath)
+        os.mkdir(dirPath)
 
-	configPath=os.path.join(dirPath,"userConfig.json")
-	if not override:
-		if os.path.exists(configPath):
-			answer=input("Configuration file %s already exist, override ([Y]/N) ?"%configPath)
-			if str(answer).lower() in ["n","no"]:
-				return
+    configPath = os.path.join(dirPath, "userConfig.json")
+    if not override:
+        if os.path.exists(configPath):
+            answer = input("Configuration file %s already exist, override ([y]/n) ?"%configPath)
+            if str(answer).lower() in ["n", "no"]:
+                return
 
-	print("Creating configuration file ",configPath)
-	parameters= {"path_to_data":"/data",
-				 "path_to_back_up":"/NAS02",
-				 "length_ID":3,
-				 "dateTime_formats":["_yyyy_MM_dd_HH_mm"],
-				 "default_ip_for_server":"10.51.25.1",
-				 "default_port_for_server":"1234",
-				 "rsync_arg_local_to_backup":["-rlzutO"],
-				 "rsync_arg_backup_to_local":["-rlzutO","--exclude=*.dat"],
-				 "window_pixel_width":1000,
-				 "window_pixel_height":1000,
-				 }
-	
-	with open(configPath, "w") as f:
-		json.dump(parameters,f,sort_keys=True,indent=4)
+    print("Creating configuration file ", configPath)
+    parameters= {"path_to_data": "/data",
+                 "path_to_back_up": "/NAS02",
+                 "length_ID": 3,
+                 "dateTime_formats": ["_yyyy_MM_dd_HH_mm"],
+                 "default_ip_for_server": "10.51.25.1",
+                 "default_port_for_server": "1234",
+                 "rsync_arg_local_to_backup": ["-rlzutO"],
+                 "rsync_arg_backup_to_local": ["-rlzutO","--exclude=*.dat"],
+                 "window_pixel_width": 1000,
+                 "window_pixel_height": 1000,
+                 }
+    
+    with open(configPath, "w") as f:
+        json.dump(parameters, f, sort_keys=True, indent=4)
 
-# Get path to user's root
 def get_user_folder_path():
-	homePath=os.path.expanduser("~")
-	dirPath=os.path.join(homePath,"processManager")
-	return dirPath
+    """Get path to user's root"""
+    
+    homePath = os.path.expanduser("~")
+    dirPath = os.path.join(homePath, "processManager")
+    return dirPath
 
-#Get path to user's config file
 def get_user_config_path():
-	dirPath=get_user_folder_path()
-	configPath=os.path.join(dirPath,"userConfig.json")
-	return configPath
+    """Get path to user's config file"""
+    
+    dirPath = get_user_folder_path()
+    configPath = os.path.join(dirPath, "userConfig.json")
+    return configPath
 
-#Get path the user's scripts folder
-def get_user_script_path():
-	dirPath=get_user_folder_path()
-	scriptPath=os.path.join(dirPath,"scripts")
-	return scriptPath
-
-#Read the json configuration file, return dictionnary
 def read_user_config_file():
-	configPath=get_user_config_path()
-	if not os.path.exists(configPath):
-		return None
-	else:
-		with open(configPath,'r') as f:
-			parameters=json.load(f)
-		return parameters
-	
+    """Read the json configuration file, return dictionnary"""
+    
+    configPath = get_user_config_path()
+    if not os.path.exists(configPath):
+        return None
+    else:
+        with open(configPath, 'r') as f:
+            parameters = json.load(f)
+        return parameters
+    
 def get_klusta_path():
-	homePath=os.path.expanduser("~")
-	anaconda=os.path.join(homePath,"anaconda/envs/klusta/bin/klusta")
-	if os.path.exists(anaconda):
-		return anaconda
-	miniconda=os.path.join(homePath,"miniconda/envs/klusta/bin/klusta")
-	if os.path.exists(miniconda):
-		return miniconda
-	anaconda3=os.path.join(homePath,"anaconda3/envs/klusta/bin/klusta")
-	if os.path.exists(anaconda3):
-		return anaconda3
-	miniconda3=os.path.join(homePath,"miniconda3/envs/klusta/bin/klusta")
-	if os.path.exists(miniconda3):
-		return miniconda3
-	return "klusta"
+    homePath = os.path.expanduser("~")
+    potentialPaths = ["anaconda/envs/klusta/bin/klusta", "anaconda3/envs/klusta/bin/klusta",
+                      "miniconda/envs/klusta/bin/klusta", "miniconda3/envs/klusta/bin/klusta"]
+    for path in potentialPaths:
+        full = os.path.join(homePath, path)
+        if os.path.exists(full):
+            return full
+    return "klusta"
 
 
 #------------------------------------------------------------------------------------------
 #    Main Window   
 #------------------------------------------------------------------------------------------
-TITLE="Klusta Process Manager"
+TITLE = "Klusta Process Manager"
 
 #------------------------------------------------------------------------------------------
 #    Database
 #------------------------------------------------------------------------------------------
-DEFAULT_ICON="folder-grey.png"
+DEFAULT_ICON = "folder-grey.png"
 
 #------------------------------------------------------------------------------------------
 # Server
 #------------------------------------------------------------------------------------------
-RSYNC_ARG_FROM_BACKUP_TO_SERVER=["-rlzutO","--prune-empty-dirs","--include","*/","--include=*.prm","--include=*.prb","--include=*.dat","--exclude=*"]
+RSYNC_ARG_FROM_BACKUP_TO_SERVER = ["-rlzutO","--prune-empty-dirs","--include","*/","--include=*.prm",
+                                   "--include=*.prb","--include=*.dat","--exclude=*"]
 
-SERVER_PATH="/home/david/Code/application/test/dataServer"
-BACK_UP_PATH='/home/david/Code/application/test/fakeNAS'
-PORT=1234
+SERVER_PATH = "/home/david/Code/application/test/dataServer"
+BACK_UP_PATH = '/home/david/Code/application/test/fakeNAS'
+PORT = 1234
 
 
 
--- klusta_process_manager/experiment/experiment.py
+++ klusta_process_manager/experiment/experiment.py
@@ -10,155 +10,155 @@ from .klustaFolder import KlustaFolder
 # Experiment (=one folder)
 #----------------------------------------------------------------------------------------------
 class Experiment(QtCore.QObject):
-	
-	#Init an experiment from a dictionnary (result of a database query)
-	def __init__(self,expInfoDict, parent=None):
-		super(Experiment,self).__init__(parent)
-		self.state="--"
-		self.isValid=True
-		
-		#Database related
-		self.hasChange=False  # True = have to be update in database
-		
-		#Information contained in database
-		self.folderName=expInfoDict["folderName"]
-		icon=expInfoDict["icon"]
-		self.pathBackUP=expInfoDict["pathBackUP"]
-		self.pathLocal=QtCore.QFileInfo(expInfoDict["pathLocal"]).absoluteFilePath()
-		self.animalID=expInfoDict["animalID"]
-		try:
-			self.yearMonth=expInfoDict["yearMonth"]
-			self.day=expInfoDict["day"]
-			self.time=expInfoDict["time"]
-		except KeyError: #server side
-			self.yearMonth="--"
-			self.day="--"
-			self.time="--"
+    
+    #Init an experiment from a dictionnary (result of a database query)
+    def __init__(self, expInfoDict, parent=None):
+        super(Experiment,self).__init__(parent)
+        self.state = "--"
+        self.isValid = True
+        
+        #Database related
+        self.hasChange = False  # True = have to be updated in database
+        
+        #Information contained in database
+        self.folderName = expInfoDict["folderName"]
+        icon = expInfoDict["icon"]
+        self.pathBackUP = expInfoDict["pathBackUP"]
+        self.pathLocal = QtCore.QFileInfo(expInfoDict["pathLocal"]).absoluteFilePath()
+        self.animalID = expInfoDict["animalID"]
+        try:
+            self.yearMonth = expInfoDict["yearMonth"]
+            self.day = expInfoDict["day"]
+            self.time = expInfoDict["time"]
+        except KeyError: #server side
+            self.yearMonth = "--"
+            self.day = "--"
+            self.time = "--"
 
-		#retrieve date for sorting purpose
-		dateString=self.yearMonth+self.day+self.time
-		self.dateTime=QtCore.QDateTime().fromString(dateString," MMM \n yyyy  ddd dd  hh:mm ")
-			
-		#Local folder
-		if not QtCore.QDir(self.pathLocal).exists():
-			self.state="Could not find folder %s"%self.pathLocal
-			self.isValid=False
-			return
-		self.folder=KlustaFolder(self.pathLocal,icon)
+        #retrieve date for sorting purpose
+        dateString = self.yearMonth + self.day + self.time
+        self.dateTime = QtCore.QDateTime().fromString(dateString, " MMM \n yyyy  ddd dd  hh:mm ")
+            
+        #Local folder
+        if not QtCore.QDir(self.pathLocal).exists():
+            self.state = "Could not find folder %s"%self.pathLocal
+            self.isValid = False
+            return
+        self.folder = KlustaFolder(self.pathLocal,icon)
 
-		if QtCore.QDir(self.pathBackUP).exists():
-			self.backUPFolder=KlustaFolder(self.pathBackUP)
-		else:
-			self.backUPFolder=None
-			self.state="Could not find folder %s in back up" %(self.folderName)
-			return
+        if QtCore.QDir(self.pathBackUP).exists():
+            self.backUPFolder = KlustaFolder(self.pathBackUP)
+        else:
+            self.backUPFolder = None
+            self.state = "Could not find folder %s in back up" %(self.folderName)
+            return
 
-	#comparison between object (lt=less than)
-	def __lt__(self,other):
-		return self.dateTime<other.dateTime
+    #comparison between object (lt=less than)
+    def __lt__(self, other):
+        return self.dateTime < other.dateTime
 
-	#Automaticaly find a icon for the folder
-	def reset_folder_icon(self):
-		previousIcon=self.folder.icon
-		self.folder.reset_icon()
-		if self.folder.icon!=previousIcon:
-			self.hasChange=True
+    #Automaticaly find a icon for the folder
+    def reset_folder_icon(self):
+        previousIcon = self.folder.icon
+        self.folder.reset_icon()
+        if self.folder.icon != previousIcon:
+            self.hasChange = True
 
-	def refresh_state(self):
-		if (self.backUPFolder is None) and (QtCore.QDir(self.pathBackUP).exists()):
-			self.backUPFolder=KlustaFolder(self.pathBackUP)
-		if (self.backUPFolder is None) or (not self.backUPFolder.exists()):
-			self.state="Could not find folder %s in backUP"%(self.folderName)
-		elif not self.folder.exists():
-			self.state="Could not find folder at %s"%self.pathLocal
-		elif self.folder.has_kwik():
-			self.state="Done (kwik file)"
-		elif self.backUPFolder.has_kwik():
-			self.state="Done (kwik file on back up)"
-		elif self.folder.can_be_process():
-			self.state="ready to be processed"
-		else:
-			self.state=self.folder.state
-			if not self.folder.has_rawData():
-				if self.backUPFolder.has_rawData():
-					self.state="raw data on back up"
+    def refresh_state(self):
+        if (self.backUPFolder is None) and (QtCore.QDir(self.pathBackUP).exists()):
+            self.backUPFolder = KlustaFolder(self.pathBackUP)
+        if (self.backUPFolder is None) or (not self.backUPFolder.exists()):
+            self.state = "Could not find folder %s in backUP"%(self.folderName)
+        elif not self.folder.exists():
+            self.state = "Could not find folder at %s"%self.pathLocal
+        elif self.folder.has_kwik():
+            self.state = "Done"
+        elif self.backUPFolder.has_kwik():
+            self.state = "Done (kwik file on back up)"
+        elif self.folder.can_be_process():
+            self.state = "ready to be processed"
+        else:
+            self.state = self.folder.state
+            if not self.folder.has_rawData():
+                if self.backUPFolder.has_rawData():
+                    self.state = "raw data on back up"
 
-	def create_files(self,prmModel,prbModel):
-		if self.folder.has_rawData():
-			self.folder.create_files(prmModel=prmModel,prbModel=prbModel)
-		elif self.backUPFolder.has_rawData():
-			rawData=self.backUPFolder.rawData.fileName()
-			self.folder.create_files(prmModel=prmModel,prbModel=prbModel,rawData=rawData)
-		else:
-			self.state="raw data not found"
+    def create_files(self, prmModel, prbModel):
+        if self.folder.has_rawData():
+            self.folder.create_files(prmModel=prmModel, prbModel=prbModel)
+        elif self.backUPFolder.has_rawData():
+            rawData = self.backUPFolder.rawData.fileName()
+            self.folder.create_files(prmModel=prmModel, prbModel=prbModel, rawData=rawData)
+        else:
+            self.state = "raw data not found"
 
-	def is_done_and_backUP(self):
-		return self.backUPFolder.has_kwik()
+    def is_done_and_backUP(self):
+        return self.backUPFolder.has_kwik()
 
-	#------------------------------------------------------------------------------------------------------
-	#Processs local
-	def can_be_process(self):
-		if (self.backUPFolder is None) or (not self.backUPFolder.exists()):
-			self.state="Could not find folder %s in BACK_UP"%self.folderName
-			return False
-		elif not self.folder.can_be_process():
-			self.state=self.folder.state
-			return False
-		elif self.backUPFolder.has_kwik():
-			self.state="Done (kwik file on back up)"
-			return False
-		return True
+    #-------------------------------------------------------------------------------------------------
+    #Processs local
+    def can_be_process(self):
+        if (self.backUPFolder is None) or (not self.backUPFolder.exists()):
+            self.state = "Could not find folder %s in BACK_UP"%self.folderName
+            return False
+        elif not self.folder.can_be_process():
+            self.state = self.folder.state
+            return False
+        elif self.backUPFolder.has_kwik():
+            self.state = "Done (kwik file on back up)"
+            return False
+        return True
 
-	def can_be_process_on_server(self):
-		if self.can_be_process():
-			return True
-		elif self.backUPFolder.can_be_process():
-			return True
-		#dat on BackUP, prm and prb on local
-		elif self.backUPFolder.has_rawData():
-			if self.folder.prm.exists() and self.folder.prb.exists():
-				return True
-		return False
+    def can_be_process_on_server(self):
+        if self.can_be_process():
+            return True
+        elif self.backUPFolder.can_be_process():
+            return True
+        #dat on BackUP, prm and prb on local
+        elif self.backUPFolder.has_rawData():
+            if self.folder.prm.exists() and self.folder.prb.exists():
+                return True
+        return False
 
-	def run_process(self,process):
-		res=self.folder.run_process(process)
-		self.state=self.folder.state
-		return res
+    def run_process(self, process):
+        res = self.folder.run_process(process)
+        self.state = self.folder.state
+        return res
 
-	def process_is_done(self,exitcode):
-		res=self.folder.is_done(exitcode)
-		self.state=self.folder.state
-		return res
-	
-	#------------------------------------------------------------------------------------------------------
-	# Sync (one at a time)
-	def sync_to_backUP(self,process,arg):
-		if self.folder.exists():
-			if self.backUPFolder.exists():
-				process.start("rsync",arg+[self.pathLocal+"/",self.pathBackUP])
-				self.state="Sync Local->BackUP"
-				return True
-			self.state="No folder in backUP"
-		else:
-			self.state="No folder in local"
-		return False
-		
-	def sync_from_backUP(self,process,arg):
-		if self.folder.exists():
-			if self.backUPFolder.exists():
-				process.start("rsync",arg+[self.pathBackUP+"/",self.pathLocal])
-				self.state="Sync BackUP->Local"
-				return True
-			self.state="No folder in backUP"
-		else:
-			self.state="No folder in local"
-		return False
-	
-	def sync_done(self,exitcode):
-		self.folder.refresh()
-		if exitcode!=0:
-			self.state+=": FAIL, exitcode=%i"%exitcode
-			return False
-		else:
-			self.state+=": Done"
-			return True
+    def process_is_done(self,exitcode):
+        res = self.folder.is_done(exitcode)
+        self.state = self.folder.state
+        return res
+    
+    #-------------------------------------------------------------------------------------------------
+    # Sync (one at a time)
+    def sync_to_backUP(self, process, arg):
+        if self.folder.exists():
+            if self.backUPFolder.exists():
+                process.start("rsync", arg + [self.pathLocal+"/", self.pathBackUP])
+                self.state = "Sync Local->BackUP"
+                return True
+            self.state = "No folder in backUP"
+        else:
+            self.state = "No folder in local"
+        return False
+        
+    def sync_from_backUP(self,process,arg):
+        if self.folder.exists():
+            if self.backUPFolder.exists():
+                process.start("rsync", arg + [self.pathBackUP+"/", self.pathLocal])
+                self.state="Sync BackUP->Local"
+                return True
+            self.state = "No folder in backUP"
+        else:
+            self.state = "No folder in local"
+        return False
+    
+    def sync_done(self,exitcode):
+        self.folder.refresh()
+        if exitcode != 0:
+            self.state += ": FAIL, exitcode=%i"%exitcode
+            return False
+        else:
+            self.state += ": Done"
+            return True
